061854ca3d431e8f4823e47d21494666bf99b4d8
Colve <573582096@qq.com>
2019-03-18 01:02:40 +0800

    feat: all

---
+++ Dijkstra/Assets/Scripts/FindPath.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPath : MonoBehaviour
+{
+	private List<Node> allNodes;
+	private Dictionary<int, float> disDic = new Dictionary<int, float>();
+	private Dictionary<int, List<int>> pathDic = new Dictionary<int, List<int>>();
+	private List<int> hasFindList = new List<int>();
+	
+	public void Find(Node startNode, Node endNode, List<Node> all)
+	{
+		allNodes = all;
+		all.ForEach(x =>
+		{
+			disDic[x.id] = 999999;
+			pathDic[x.id] = new List<int>();
+		});
+		disDic[startNode.id] = 0;
+		pathDic[startNode.id].Add(startNode.id);
+		FindNode(startNode);
+
+		string path = "";
+		pathDic[endNode.id].ForEach(x => path += x + "_");
+		Debug.Log("start:" + startNode.id + " end:" + endNode.id +" path:" + path);
+		
+		disDic.Clear();
+		pathDic.Clear();
+		hasFindList.Clear();
+	}
+
+	public void FindNode(Node node)
+	{
+		hasFindList.Add(node.id);
+		node.neighborList.ForEach(x =>
+		{
+			float dis = disDic[node.id] + Vector3.Distance(x.pos, node.pos);
+			if (dis < disDic[x.id])
+			{
+				disDic[x.id] = dis;
+				pathDic[x.id].Clear();
+				pathDic[node.id].ForEach(y => pathDic[x.id].Add(y));
+				pathDic[x.id].Add(x.id);
+			}
+		});
+		Node nearNode = FindNearNode();
+		if (nearNode == null)
+		{
+			return;
+		}
+		FindNode(nearNode);
+	}
+
+	private Node FindNearNode()
+	{
+		Node node = null;
+		float dis = 999999;
+		allNodes.ForEach(x =>
+		{
+			if (!hasFindList.Contains(x.id) && disDic[x.id] < dis)
+			{
+				node = x;
+				dis = disDic[x.id];
+			}
+		});
+		return node;
+	}
+
+}
---
+++ Dijkstra/Assets/Scripts/TestController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+	public Material red;
+	public Material green;
+	public Material blue;
+
+	public int startIndex;
+	public int endIndex;
+	
+	
+	[ContextMenu("Change")]
+	public void ChangePos()
+	{
+		transform.Find("Nodes").GetComponentsInChildren<Node>().ToList().ForEach(x =>
+			{
+				x.pos = x.gameObject.transform.position;
+			});
+	}
+	
+	[ContextMenu("Connect")]
+	public void Connect()
+	{
+		transform.Find("Nodes").GetComponentsInChildren<Node>().ToList().ForEach(x =>
+		{
+			x.neighborList.ForEach(y =>
+			{
+				if (!y.neighborList.Contains(x))
+				{
+					y.neighborList.Add(x);
+				}
+			});
+			x.id = x.transform.GetSiblingIndex();
+		});
+	}
+
+	[ContextMenu("DrawLine")]
+	public void DrawLine()
+	{
+		for (int i = transform.Find("Lines").childCount - 1; i >= 0; i--)
+		{
+			DestroyImmediate(transform.Find("Lines").GetChild(i).gameObject);
+		}
+		
+		for (int i = transform.Find("Words").childCount - 1; i >= 0; i--)
+		{
+			DestroyImmediate(transform.Find("Words").GetChild(i).gameObject);
+		}
+		
+		List<string> nameList = new List<string>();
+		transform.Find("Nodes").GetComponentsInChildren<Node>().ToList().ForEach(x =>
+		{
+			x.neighborList.ForEach(y =>
+			{
+				if (!nameList.Contains(y.id + "_" + x.id))
+				{
+					GameObject line = new GameObject();
+					line.transform.SetParent(transform.Find("Lines"));
+					line.AddComponent<LineRenderer>();
+					line.GetComponent<LineRenderer>().SetPositions(new []{x.pos, y.pos});
+					line.GetComponent<LineRenderer>().startWidth = 0.1f;
+					line.GetComponent<LineRenderer>().endWidth = 0.1f;
+					line.GetComponent<LineRenderer>().material = red;
+					line.name = x.id + "_" + y.id;
+					nameList.Add(line.name);
+				
+					GameObject word = new GameObject();
+					word.transform.SetParent(transform.Find("Words"));
+					word.AddComponent<TextMesh>();
+					word.transform.position = x.pos + ((y.pos - x.pos) / 2);
+					word.GetComponent<TextMesh>().text = Vector3.Distance(x.pos, y.pos).ToString("F");
+					word.GetComponent<TextMesh>().characterSize = 0.1f;
+					word.transform.Rotate(new Vector3(90,0,0));
+				}
+			});
+			GameObject nameWord = new GameObject();
+			nameWord.transform.SetParent(transform.Find("Words"));
+			nameWord.AddComponent<TextMesh>();
+			nameWord.transform.position = x.pos;
+			nameWord.GetComponent<TextMesh>().text = x.id.ToString();
+			nameWord.GetComponent<TextMesh>().characterSize = 0.1f;
+			nameWord.GetComponent<TextMesh>().color = Color.red;
+			nameWord.transform.Rotate(new Vector3(90,0,0));
+		});
+	}
+
+	[ContextMenu("FindPath")]
+	public void FindPath()
+	{
+		List<Node> nodes = transform.Find("Nodes").GetComponentsInChildren<Node>().ToList();
+		Node startNode = nodes.Find(x => x.id == startIndex);
+		Node endNode = nodes.Find(x => x.id == endIndex);
+		GetComponent<FindPath>().Find(startNode, endNode, nodes);
+	}
+}
